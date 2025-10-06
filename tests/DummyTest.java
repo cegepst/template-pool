275154ab47698dbc67f2ad8c8c0a616c56f27be2
dtucker <davidt2003@msn.com>
2025-10-06 08:27:16 -0400

    Initial commit

---
+++ tests/DummyTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DummyTest {
+
+    @Test
+    public void testDummy() {
+        int result = 1 + 1;
+        assertEquals(2, result);
+    }
+}
